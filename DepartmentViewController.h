d826aa60f0616b04aba3a8ecdccda1b9a2119a4b
Charles Feduke <charles.feduke@gmail.com>
2011-02-24 15:40:39 -0500

    Department view and UI

---
+++ DepartmentViewController.h
@@ -0,0 +1,16 @@
+//
+//  DepartmentViewController.h
+//  Departments
+//
+//  Created by Charles Feduke on 2/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "ManagingViewController.h"
+
+@interface DepartmentViewController : ManagingViewController {
+	
+}
+
+@end
---
+++ DepartmentViewController.m
@@ -0,0 +1,14 @@
+//
+//  DepartmentViewController.m
+//  Departments
+//
+//  Created by Charles Feduke on 2/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DepartmentViewController.h"
+
+
+@implementation DepartmentViewController
+
+@end
