4e3d22321e21163db791c5462073d9d912dcc42a
Charles Feduke <charles.feduke@gmail.com>
2011-02-24 15:27:24 -0500

    initial commit

---
+++ MyDocument.h
@@ -0,0 +1,14 @@
+//
+//  MyDocument.h
+//  Departments
+//
+//  Created by Charles Feduke on 2/24/11.
+//  Copyright __MyCompanyName__ 2011 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSPersistentDocument {
+}
+
+@end
---
+++ MyDocument.m
@@ -0,0 +1,33 @@
+//
+//  MyDocument.m
+//  Departments
+//
+//  Created by Charles Feduke on 2/24/11.
+//  Copyright __MyCompanyName__ 2011 . All rights reserved.
+//
+
+#import "MyDocument.h"
+
+@implementation MyDocument
+
+- (id)init 
+{
+    self = [super init];
+    if (self != nil) {
+        // initialization code
+    }
+    return self;
+}
+
+- (NSString *)windowNibName 
+{
+    return @"MyDocument";
+}
+
+- (void)windowControllerDidLoadNib:(NSWindowController *)windowController 
+{
+    [super windowControllerDidLoadNib:windowController];
+    // user interface preparation code
+}
+
+@end
